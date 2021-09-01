08b6e0a7369bfdeac412ddce4e15b739da68861e
Solomon Kassahun <se.solomon.kasahun@gmail.com>
2021-09-01 13:36:01 +0300

    base of project

--- dating_app/README.md
+++ dating_app/README.md
@@ -1,4 +1,4 @@
-# dating
+# dating_app
 
 A new Flutter project.
 
--- dating_app/android/app/src/main/kotlin/com/example/dating_app/MainActivity.kt
+++ dating_app/android/app/src/main/kotlin/com/example/dating_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.dating
+package com.example.dating_app
 
 import io.flutter.embedding.android.FlutterActivity
 
